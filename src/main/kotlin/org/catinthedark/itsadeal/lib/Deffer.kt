5987206c72de72d9768c856321fb09ffd41e2b98
BOOtak <kolbasisha@gmail.com>
2019-04-28 17:56:18 +0300

    Fix deffer.

--- src/main/kotlin/org/catinthedark/itsadeal/lib/Deffer.kt
+++ src/main/kotlin/org/catinthedark/itsadeal/lib/Deffer.kt
@@ -1,8 +1,13 @@
 package org.catinthedark.itsadeal.lib
 
+import org.slf4j.LoggerFactory
+import java.util.concurrent.ConcurrentHashMap
+
 class Deffer {
-    private val funcs: MutableMap<Long, Func> = mutableMapOf()
+    private val funcs: ConcurrentHashMap<Long, Func> = ConcurrentHashMap()
     private var index: Long = 0L
+
+    private val log = LoggerFactory.getLogger(Deffer::class.java)
 
     fun register(time: Float, func: () -> Unit): Long {
         funcs[index++] = Func(func, time)
@@ -19,6 +24,7 @@ class Deffer {
 
             if (it.value.time <= 0) {
                 it.value.func()
+                log.info("remove func from deffer at ${Thread.currentThread().name}")
                 funcs.remove(it.key)
             }
         }
