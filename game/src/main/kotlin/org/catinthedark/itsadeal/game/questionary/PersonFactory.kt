0a3a6e51333eac8b188d7b7a4b356944e271ab14
BOOtak <kolbasisha@gmail.com>
2019-04-29 23:58:41 +0300

    Add more questions.

--- game/src/main/kotlin/org/catinthedark/itsadeal/game/questionary/PersonFactory.kt
+++ game/src/main/kotlin/org/catinthedark/itsadeal/game/questionary/PersonFactory.kt
@@ -3,7 +3,7 @@ package org.catinthedark.itsadeal.game.questionary
 import kotlin.random.Random
 
 class PersonFactory {
-    private val INCRIMINATING_PART = 0.4f
+    private val INCRIMINATING_PART = 1f
 
     fun getRandomPerson(): Person {
         return Person(Random(System.currentTimeMillis()).nextBoolean(), INCRIMINATING_PART)
