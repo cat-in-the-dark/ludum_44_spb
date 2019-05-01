63ab45dc8da4561b8ee785b62d7211f81a9c006d
Kirill Leyfer <kolbasisha@gmail.com>
2019-05-01 19:49:37 +0300

    Replace true random with the random poll.

--- game/src/main/kotlin/org/catinthedark/itsadeal/game/questionary/PersonFactory.kt
+++ game/src/main/kotlin/org/catinthedark/itsadeal/game/questionary/PersonFactory.kt
@@ -9,15 +9,27 @@ import kotlin.random.Random
 class PersonFactory {
 
     // TODO: generate more random pools for other levels
-    private val randomPool: MutableList<Boolean> = mutableListOf(1, 1, 0).apply {
+    private val initialRandomPool: MutableList<Boolean> = mutableListOf(1, 1, 0).apply {
         shuffle()
     }.map { it == 0 }.toMutableList()
+
+    private val randomPool: MutableList<Boolean> = mutableListOf()
+
+    private fun putNextPool(list: MutableList<Boolean>) {
+        list.addAll(listOf(1, 1, 1, 0, 0, 0).map { it == 0 }.shuffled())
+    }
 
     fun getRandomPerson(): Person {
         val money = IOC.atOr("money", 0)
         val txt: Texts by IOC
 
-        val isBad = if (randomPool.isNotEmpty()) randomPool.removeAt(0) else nextBool()
+        val isBad = if (initialRandomPool.isNotEmpty()) initialRandomPool.removeAt(0)
+        else {
+            if (randomPool.isEmpty()) {
+                putNextPool(randomPool)
+            }
+            randomPool.removeAt(0)
+        }
         return Person(isBad, incriminatingFactor(money), txt)
     }
 
